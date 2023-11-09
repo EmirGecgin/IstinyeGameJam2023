7a4bbb404cef548d0d8d818a8b1f8ecbce20f135
LAPTOP-DU3HUBBS\EMİR <gecginemir@gmail.com>
2023-11-09 23:06:47 +0300

    +Jam Game is done.

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private bool _wasCollected = false;
+    public AudioClip collectSong;
+
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && !_wasCollected)
+        {
+            _wasCollected = true;
+            FindObjectOfType<PlayerStateManager>().TakeCoin(10);
+            GetComponent<AudioSource>().PlayOneShot(collectSong);
+            Destroy(gameObject,0.16f);
+            
+
+        }
+    }
+    
+}
